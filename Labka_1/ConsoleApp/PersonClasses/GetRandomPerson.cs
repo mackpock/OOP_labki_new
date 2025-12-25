bc69ca716d46c245c4ac85597c0c7961560f0a29
mackpock <mackpock1337@gmail.com>
2025-12-25 19:59:22 +0700

    TODO: выполнено

--- Labka_1/ConsoleApp/PersonClasses/GetRandomPerson.cs
+++ Labka_1/ConsoleApp/PersonClasses/GetRandomPerson.cs
@@ -11,37 +11,53 @@ namespace PersonClasses
     /// </summary>
     public static class GetRandomPersonClass
     {
-        //TODO: RSDN 
-
+        //TODO: RSDN +
+        /// <summary>
+        /// Минимальный возраст
+        /// </summary>
         private const int MinAge = 0;
 
-        //TODO: RSDN 
+        //TODO: RSDN +
 
+        /// <summary>
+        /// Максимальный возраст
+        /// </summary>
         private const int MaxAge = 124;
         
-        //TODO: RSDN 
+        //TODO: RSDN +
+
+        /// <summary>
+        /// Мужские имена
+        /// </summary>
         private static readonly string[] MaleNames =
         {
             "Артём",  "Кирилл", "Егор", "Тимофей", "Михаил",
             "Степан", "Глеб",   "Лев",  "Фёдор",   "Платон"
         };
 
-        //TODO: RSDN 
-
+        //TODO: RSDN +
+        /// <summary>
+        /// Женские имена
+        /// </summary>
         private static readonly string[] FemaleNames =
         {
             "София", "Полина", "Алиса", "Ева", "Милана",
             "Арина", "Ульяна", "Дарья", "Вера"
         };
 
-        //TODO: RSDN 
-
+        //TODO: RSDN +
+        /// <summary>
+        /// Мужские фамилии
+        /// </summary>
         private static readonly string[] MaleSurnames =
         {
             "Морозов", "Белов",  "Крылов",  "Ермаков", "Федоров",
             "Лебедев", "Громов", "Соколов", "Макаров", "Никитин"
         };
 
+        /// <summary>
+        /// Женские фамилии
+        /// </summary>
         private static readonly string[] FemaleSurnames =
         {
             "Морозова", "Белова",  "Крылова",  "Ермакова", "Федорова",
